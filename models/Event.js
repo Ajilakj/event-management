8530a3ee429473a76ac1b5375f2b1c91e5ba49fa
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-08 12:24:23 -0500

    create db and event model

---
+++ models/Event.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+  {
+    event_id : {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    event_title: {
+      type: DataTypes.STRING
+    },
+    event_location: {
+      type: DataTypes.STRING
+    },
+    event_data: {
+      type: DataTypes.JSON
+    },
+    event_tag: {
+      type: DataTypes.STRING
+    },
+    event_date: {
+      type: DataTypes.DATE
+    },
+    event_image: {
+      type: DataTypes.JSON
+    },
+    event_date_created: {
+        type: DataTypes.DATE
+    },
+    client_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Event'
+  }
+);
+
+module.exports = Event;
