aad5843d4317d1aceb234ef95f655e934343190f
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-10 17:15:21 -0500

    add cors origin:'*'

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
@@ -10,21 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-});
+
+app.use(cors({
+  origin: '*'
+}));
+
 // app.get("/",function(req,res){
 //   res.send("Hello world deployment")
 // })
