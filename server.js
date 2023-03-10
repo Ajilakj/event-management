b99dacb111e06cf3202d7cb6b34c50029aa16401
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-10 16:47:11 -0500

    add res.header for CORS

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+});
 // app.get("/",function(req,res){
 //   res.send("Hello world deployment")
 // })
