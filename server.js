2e7b92a6ff6916dbdeff11ab7cbbe148776c6f0e
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-10 21:52:32 -0500

    add res.setHeader for CORS

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
 const express = require('express');
-const cors = require('cors');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
+
+
 const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
-app.use(cors());
-
-// app.get("/",function(req,res){
-//   res.send("Hello world deployment")
-// })
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("test", "test");
-  console.log("test..");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
+
+
+// your app routes go here...
+app.use(routes);
+
+app.listen(process.env.PORT || 3000);
 
 // Force false so data doesn't get dropped on every sync
 sequelize.sync({ force: false }).then(() => {
