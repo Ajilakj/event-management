c1bc564240c4cb3c171189d401b71d37d723a7a3
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-27 19:46:18 -0400

    add route for create new event

--- routes/api/eventRoutes.js
+++ routes/api/eventRoutes.js
@@ -162,6 +162,18 @@ router.get('/:eventID', (req, res) => {
   });
 });
 
+
+// create new event
+router.post('/', async (req, res) => {
+  try {
+    const eventData = await Event.create(req.body);
+    res.status(200).json(eventData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
 // router.get('/search/:title', (req, res) => {
 //     // Get all event from the event table if location matches
 //     Event.findAll(
